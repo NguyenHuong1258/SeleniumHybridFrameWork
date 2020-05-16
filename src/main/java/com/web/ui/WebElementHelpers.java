cba4b40be59ce08bffd1565ad47ca6b76b99fd59
NguyenHuong1258 <42576811+NguyenHuong1258@users.noreply.github.com>
2020-05-16 16:35:33 +0700

    Done WebElementHelpers/Click

--- src/main/java/com/web/ui/WebElementHelpers.java
+++ src/main/java/com/web/ui/WebElementHelpers.java
@@ -1,5 +1,6 @@
 package com.web.ui;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
@@ -23,4 +24,14 @@ public abstract class WebElementHelpers extends WebElementVerifications {
         LOGGER.info(String.format("Send keys: %s into text box", valueToType));
         editBox.sendKeys(valueToType);
     }
+
+    //Click Command by WebElement
+    public void clickButton(final WebElement clickbtn){
+        waitForElementToBeClickable(clickbtn);
+        LOGGER.info("Button is clicked.");
+        clickbtn.click();
+    }
+
+
+
 }
