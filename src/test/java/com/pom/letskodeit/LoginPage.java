9635813f8fb67f8fe3d47ac6fe779908d762fcce
ginggon307@gmail.com <ginggon307@gmail.com>
2020-05-16 21:13:59 +0700

    Login page

--- src/test/java/com/pom/letskodeit/LoginPage.java
+++ src/test/java/com/pom/letskodeit/LoginPage.java
@@ -1,14 +1,10 @@
 package com.pom.letskodeit;
 
 import com.basepage.AbstractBasePage;
-import com.testcases.LoginTest;
-import org.apache.logging.log4j.LogManager;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-import org.testng.log4testng.Logger;
 
 public class LoginPage extends AbstractBasePage {
     public LoginPage(WebDriver driver) {
@@ -19,9 +15,6 @@ public class LoginPage extends AbstractBasePage {
     /***
     * Locators
     * */
-    //private By emailField = By.xpath("//*[@id='user_email']");
-    //private By passWordField = By.xpath("//*[@id='user_password']");
-    //private By submitButton = By.xpath("//*[@type='submit']");
 
     @FindBy(xpath = "//*[@id='user_email']")
     private WebElement emailField;
@@ -41,7 +34,7 @@ public class LoginPage extends AbstractBasePage {
     public void login(String email, String password) {
         sendKeyToTextBox(emailField, email);
         sendKeyToTextBox(passWordField, password);
-        clickElement(submitButton);
+
     }
 
 
